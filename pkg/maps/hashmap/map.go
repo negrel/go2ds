193bb37c32c5abc6228a87c29e7a1aed1a6f2333
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-13 11:14:33 +0100

    adding comments for Map methods

--- pkg/maps/hashmap/map.go
+++ pkg/maps/hashmap/map.go
@@ -7,10 +7,12 @@ import (
 
 var _ maps.Map = &Map{}
 
+// Map is a wrapper around the built-in map type.
 type Map struct {
 	m map[maps.Key]maps.Value
 }
 
+// New instantiate a Map
 func New() maps.Map {
 	return newMap()
 }
@@ -21,19 +23,25 @@ func newMap() *Map {
 	}
 }
 
+// Put insert the given value in the map.
 func (m *Map) Put(key maps.Key, value maps.Value) {
 	m.m[key] = value
 }
 
+// Get return the element mapped to the given key.
+// A nil value and false is returned if the map doesn't contain any value
+// stored with the given key.
 func (m *Map) Get(key maps.Key) (maps.Value, bool) {
 	v, ok := m.m[key]
 	return v, ok
 }
 
+// Delete the given key in the map.
 func (m *Map) Delete(key maps.Key) {
 	delete(m.m, key)
 }
 
+// Keys return all the keys present in the map.
 func (m *Map) Keys() []maps.Key {
 	keys := make([]maps.Key, len(m.m))
 	i := 0
@@ -45,18 +53,22 @@ func (m *Map) Keys() []maps.Key {
 	return keys
 }
 
+// Empty return if the map is empty.
 func (m *Map) Empty() bool {
 	return m.Size() == 0
 }
 
+// Size return the number of elements stored in the map.
 func (m *Map) Size() int {
 	return len(m.m)
 }
 
+// Clear remove all elements stored in the map.
 func (m *Map) Clear() {
 	m.m = make(map[maps.Key]maps.Value)
 }
 
+// Values return all the values stored in the map.
 func (m *Map) Values() []elements.Element {
 	result := make([]elements.Element, m.Size())
 
